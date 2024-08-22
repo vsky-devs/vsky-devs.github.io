e661dc89b95122ab98196a575d4c65aa42159a0f
AntonPtashynskyi <a.ptashynskyi@gmail.com>
2024-08-22 21:33:19 +0200

    Adjust links

--- src/routes/[lang]/+layout.svelte
+++ src/routes/[lang]/+layout.svelte
@@ -3,15 +3,12 @@
   import { page } from "$app/stores";
   import { goto } from "$app/navigation";
   
-  import { base } from "$app/paths";
-
-
   $: ({ route } = $page.data);
 </script>
 
 <header>
-    <a href="{base}/{$locale}">{$t("common.home")}</a>
-    <a href="{base}/{$locale}/about">{$t("common.about")}</a>
+    <a href="{$locale}">{$t("common.home")}</a>
+    <a href="{$locale}/about">{$t("common.about")}</a>
 
     <select on:change="{({ target }) => goto(target.value)}">
       {#each $locales as lc}
