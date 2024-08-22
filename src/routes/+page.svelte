e2b808d97cccdeab21818d6494b9783103a40538
AntonPtashynskyi <a.ptashynskyi@gmail.com>
2024-08-22 17:06:18 +0200

    Add image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script>
+    import sticker from "$lib/assets/images/sticker.png"
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<img src={sticker} alt="Sticker"/>
