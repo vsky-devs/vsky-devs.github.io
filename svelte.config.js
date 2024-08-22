175debca7dfb2baf6bae1f51f639b1ad3df9e844
AntonPtashynskyi <a.ptashynskyi@gmail.com>
2024-08-22 15:50:08 +0200

    cahgne base path

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-			base: process.argv.includes('dev') ? '' : '/vsky-page'
+			base: process.argv.includes('dev') ? '' : '/'
 		}
 	}
 };
