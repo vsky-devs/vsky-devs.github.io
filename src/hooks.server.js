ba126b9eb56406a405d358168a74ff00ea3574d1
AntonPtashynskyi <a.ptashynskyi@gmail.com>
2024-08-23 09:07:29 +0200

    Update deploy file

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,32 +1,25 @@
 import { defaultLocale, locales, setLocale, setRoute } from '$lib/translations';
 
-/** @type {import('@sveltejs/kit').Handle} */
 export const handle = async ({ event, resolve }) => {
   const { url, request } = event;
   const { pathname } = url;
 
-  // Get the base path from the environment, if set
-  const basePath = process.env.BASE_PATH || '';
-
   // Get defined locales
   const supportedLocales = locales.get().map((l) => l.toLowerCase());
 
-  // Extract the first part of the pathname to check if it's a locale
+  // Try to get locale from `pathname`.
   let locale = supportedLocales.find((l) => l === `${pathname.match(/[^/]+?(?=\/|$)/)}`.toLowerCase());
 
-  // If the route locale is not supported
+  // If route locale is not supported
   if (!locale) {
-    // Get user preferred locale from the `Accept-Language` header
+    // Get user preferred locale
     locale = `${`${request.headers.get('accept-language')}`.match(/[a-zA-Z]+?(?=-|_|,|;)/)}`.toLowerCase();
 
     // Set default locale if user preferred locale does not match
     if (!supportedLocales.includes(locale)) locale = defaultLocale;
 
-    // Check if the basePath is already part of the pathname
-    const newPathname = `${basePath}/${locale}`;
-
     // 301 redirect
-    return new Response(undefined, { headers: { 'location': newPathname }, status: 301 });
+    return new Response(undefined, { headers: { 'location': `/${locale}${pathname}` }, status: 301 });
   }
 
   // Add html `lang` attribute
@@ -34,33 +27,6 @@ export const handle = async ({ event, resolve }) => {
     transformPageChunk: ({ html }) => html.replace(/<html.*>/, `<html lang="${locale}">`),
   });
 };
-// export const handle = async ({ event, resolve }) => {
-//   const { url, request } = event;
-//   const { pathname } = url;
-
-//   // Get defined locales
-//   const supportedLocales = locales.get().map((l) => l.toLowerCase());
-
-//   // Try to get locale from `pathname`.
-//   let locale = supportedLocales.find((l) => l === `${pathname.match(/[^/]+?(?=\/|$)/)}`.toLowerCase());
-
-//   // If route locale is not supported
-//   if (!locale) {
-//     // Get user preferred locale
-//     locale = `${`${request.headers.get('accept-language')}`.match(/[a-zA-Z]+?(?=-|_|,|;)/)}`.toLowerCase();
-
-//     // Set default locale if user preferred locale does not match
-//     if (!supportedLocales.includes(locale)) locale = defaultLocale;
-
-//     // 301 redirect
-//     return new Response(undefined, { headers: { 'location': `/${locale}${pathname}` }, status: 301 });
-//   }
-
-//   // Add html `lang` attribute
-//   return resolve({ ...event, locals: { lang: locale } }, {
-//     transformPageChunk: ({ html }) => html.replace(/<html.*>/, `<html lang="${locale}">`),
-//   });
-// };
 
 /** @type {import('@sveltejs/kit').HandleServerError} */
 export const handleError = async ({ event }) => {
